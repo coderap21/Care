772ea664a55dc335078d4bc81446789b1033185b
deepak <deepakbegrajka@gmail.com>
2017-03-06 19:18:24 -0500

    new update

---
+++ app/src/main/java/healthandcharity/socialcause/care/running.java
@@ -0,0 +1,13 @@
+package healthandcharity.socialcause.care;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class running extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running);
+    }
+}
